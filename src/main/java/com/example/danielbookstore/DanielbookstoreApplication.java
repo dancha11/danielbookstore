45b4bae361ad4101a64c44561e5cd2c74e52cf75
DANIEL CHAUVIERE <bgq007@myy.haaga-helia.fi>
2022-09-07 16:17:28 +0300

    Added database and bootstrap

--- src/main/java/com/example/danielbookstore/DanielbookstoreApplication.java
+++ src/main/java/com/example/danielbookstore/DanielbookstoreApplication.java
@@ -1,13 +1,38 @@
 package com.example.danielbookstore;
 
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.example.danielbookstore.model.Book;
+import com.example.danielbookstore.model.BookRepository;
 
 @SpringBootApplication
 public class DanielbookstoreApplication {
+	private static final Logger log = LoggerFactory.getLogger(DanielbookstoreApplication.class);
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(DanielbookstoreApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner bookDemo(BookRepository repository) {
+		return (args) -> {
+			log.info("save a couple of students");
+			repository.save(new Book("Harry Potter", "JK Rowling", 1999, "34421331", 20.0));
+			repository.save(new Book("Harry Potter", "JK Rowling", 1999, "34421331", 20.0));
+
+			log.info("fetch all books");
+			for (Book book : repository.findAll()) {
+				log.info(book.toString());
+			}
+		};
 	}
 
 }
--- src/main/java/com/example/danielbookstore/web/BookController.java
+++ src/main/java/com/example/danielbookstore/web/BookController.java
@@ -1,17 +1,61 @@
 package com.example.danielbookstore.web;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.example.danielbookstore.model.Book;
+import com.example.danielbookstore.model.BookRepository;
 
 @Controller
-@ResponseBody
 public class BookController {
+	@Autowired
+	private BookRepository repository; 
 	
-	@RequestMapping(name="/index")
-	public String listbooks() {
-		return "placeholder";
+    @RequestMapping(value= {"/", "/booklist"})
+    public String bookList(Model model) {	
+        model.addAttribute("books", repository.findAll());
+        return "booklist";
+    }
+  
+    @RequestMapping(value = "/add")
+    public String addBook(Model model){
+    	model.addAttribute("book", new Book());
+        return "addbook";
+    }     
+    
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    public String save(Book book){
+        repository.save(book);
+        return "redirect:booklist";
+    }    
+
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+    public String deleteBook(@PathVariable("id") Long bookId, Model model) {
+    	repository.deleteById(bookId);
+        return "redirect:../booklist";
+    }     
+	@RequestMapping(value = "/edit/{id}")
+    public String editBook(@PathVariable("id") Long bookId, Model model){
+    	model.addAttribute("book", repository.findById(bookId));
+        return "editbook";
+    } 
+	@GetMapping("/books")
+	public @ResponseBody List<Book> bookListRest() {
+		return (List<Book>) repository.findAll();
 	}
 
+	@GetMapping("/books/{id}")
+	public @ResponseBody Optional<Book> findBookByIdRest(@PathVariable("id") Long bookId) {
+		return repository.findById(bookId);
+	}
+    
 }
